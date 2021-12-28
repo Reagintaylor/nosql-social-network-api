45b8d3f031c60aab0ebc03996126bd6a2faa13f4
Reagin Turner <rturnerafteragnes@gmail.com>
2021-12-28 16:13:15 -0500

    Added my index.js in Models

--- models/User.js
+++ models/User.js
@@ -6,26 +6,20 @@ const thoughtSchema = new mongoose.Schema({
   salary: Number,
 });
 
-// The employeeScheme defines the shape for the employee subdocument
 const reactionSchema = new mongoose.Schema({
   name: { type: String, required: true },
   salary: Number,
 });
 
-// departmentSchema provides the shape of the parent document
-const usernameSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  // This will add a single subdocument to include the manager's information
   manager: managerSchema,
-  // This will include an array that holds all the employees' information
   employees: [employeeSchema],
   lastAccessed: { type: Date, default: Date.now },
 });
 
-// Uses mongoose.model() to create model
-const Department = mongoose.model('Department', departmentSchema);
+const User = mongoose.model('User', userSchema);
 
-// Uses model to create new instance including subdocument
 const managerData = { name: 'Taylor', salary: 80000 };
 const employeeData = [
   { name: 'Ann', salary: 40000 },
