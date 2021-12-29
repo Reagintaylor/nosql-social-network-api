aaf3a9f6a156686fd34b7b465a911f9c4606ca9f
Reagin Turner <rturnerafteragnes@gmail.com>
2021-12-29 15:01:52 -0500

    Fixed the model errors; moving onto routes

--- models/Thought.js
+++ models/Thought.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
-const { Schema, model } = require('mongoose')
+const moment = require('moment');
+const { Schema, model } = require('mongoose');
 
 const reactionSchema = new mongoose.Schema({
   reactionID: { 
       type: Schema.Types.ObjectId, 
-      default: //new Object Id 
+      default: () => new Types.ObjectId()
   },
   reactionBody: {
       type: String,
@@ -32,44 +33,29 @@ const thoughtSchema = new mongoose.Schema({
  createdAt: {
     type: Date,
     default: Date.now,
-    timestamps: true 
+    timestamps: true,
     // getter method to format the timestamp on query
+    get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
  },
-  username: {
+ username: {
     type: String,
     required: true
  },
  // array of nested documents created with reaction Schema
-  reactions: [{type: Schema.Types.ObjectId, ref: 'User'}],
-
-  {
-    toJSON: {
-      virtuals: true,
-    },
-    id: false,
-  }
+  reactions: [reactionSchema],
+}, 
+{
+ toJSON: {
+    virtuals: true,
+    getters: true
+},
+    id: false
 });
-//fix the error
 
-//virtual
 thoughtSchema.virtual('reactionCount').get(function () {
     return this.reactions.length;
 });
 
-thoughtData = {}
-
-
 const Thought = mongoose.model('Thought', thoughtSchema);
-// where does the reactionSchema go?
-
-Thought.create(
-  { name: 'Shoes', thoughts: thoughtData, reactions: reactionData },
-  (err, data) => {
-    if (err) {
-      console.error(err);
-    }
-    console.log(data);
-  }
-);
 
 module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema, model } = require('mongoose')
+// const { Thought } = require('Thought')
 
 // function for validating email
 var validateEmail = function(email) {
@@ -7,51 +8,35 @@ var validateEmail = function(email) {
     return re.test(email)
 };
 
-const userSchema = new mongoose.Schema({
-  username: { 
+const userSchema = new mongoose.Schema(
+{
+username: { 
     type: String,
     required: true,
     unique: true,
     trim: true
  },
- email: {
-     type: String,
+email: {
+    type: String,
     required: true,
     unique: true,
     validate: [validateEmail, 'Please use a valid email address']
  },
-  thoughts: [{type: Schema.Types.ObjectId, ref: 'Thought'}],
-  friends: [{type: Schema.Types.ObjectId, ref: 'User'}],
-  {
-    toJSON: {
-      virtuals: true,
+thoughts: [{type: Schema.Types.ObjectId, ref: 'Thought'}],
+friends: [{type: Schema.Types.ObjectId, ref: 'User'}],
+},
+{
+toJSON: {
+    virtuals: true,
     },
     id: false,
-  }
 });
-//fix the error
 
 UserSchema.virtual('friendCount').get(function () {
     return this.friends.length;
-  });
+});
 
 
 const User = mongoose.model('User', userSchema);
-
-
-const reactionData = [
-  { name: 'Ann', salary: 40000 },
-  { name: 'Liu', salary: 50000 },
-];
-
-User.create(
-  { name: 'Shoes', thoughts: thoughtData, reactions: reactionData },
-  (err, data) => {
-    if (err) {
-      console.error(err);
-    }
-    console.log(data);
-  }
-);
 
 module.exports = User;
